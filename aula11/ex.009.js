da88ca55614b5bd627bf9d160a51cfbf442e9702
Leandro Caracciolo <leandrocaracciolo@gmail.com>
2020-07-20 17:50:14 -0300

    Uploads do mac

---
+++ aula11/ex.009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Se o seu país é ${país}`)
+if (país != 'Brasil'){
+    console.log('então você é estrangeiro')
+} else {
+    console.log('então você é brasileiro')
+}
---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+// console.log('o console funcionou corretamente')
+
+var vel = 59.999
+console.log(`A velocidade do seu carro é de ${vel}`)
+if ( vel > 60) {
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
