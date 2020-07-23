c788554059ee724fc0928c63d3c30cd91db0ecee
Leandro Caracciolo <leandrocaracciolo@gmail.com>
2020-07-23 16:14:55 -0300

    correçoes

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,6 +2,7 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
+    hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 0 && hora < 12) {
         //BOM DIA
