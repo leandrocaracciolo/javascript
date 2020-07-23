1b8e293a3b362372edf54cab278d930e086a6742
Leandro Caracciolo <leandrocaracciolo@gmail.com>
2020-07-23 12:49:27 -0300

    Exercícios da aula 12 - parte 01

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#524330'
+    } else if (hora >= 12 && hora < 18){
+        // BOA TARDE
+        img.src = 'fototarde.png'
+        document.body.style.background = '#2b1a2c'
+    } else {
+        //BOA NOITE
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#1c333b'
+    }
+}
+
